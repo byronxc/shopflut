450f7cd1e1643787380c77c34758d64cd43b7067
Byron <47398200+byronxc@users.noreply.github.com>
2021-10-30 12:07:00 -0700

    Created Splash Screen

---
+++ lib/sliderpage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class SliderPage extends StatefulWidget {
+  const SliderPage({ Key? key }) : super(key: key);
+
+  @override
+  _SliderPageState createState() => _SliderPageState();
+}
+
+class _SliderPageState extends State<SliderPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(),
+      
+    );
+  }
+}
